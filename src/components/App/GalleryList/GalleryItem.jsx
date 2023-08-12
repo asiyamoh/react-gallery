f77641f99cd7cd6a669d38abe336074c494c3aa5
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-08-12 17:48:21 -0500

    error with axios PUT

--- src/components/App/GalleryList/GalleryItem.jsx
+++ src/components/App/GalleryList/GalleryItem.jsx
@@ -1,9 +1,24 @@
+import axios from './axios'
+
 function GalleryItem(props){
-    console.log('inside the GalleryItem', props)
+    // console.log('inside the GalleryItem', props)
+
+    const likeHandler = () => {
+        console.log('the id is:', props.galleryList.id)
+        // axios.put(`/gallery/like/${props.galleryList.id}`)
+        //     .then((response) => {
+        //         console.log('The response', response)
+        //     }).catch((error) => {
+        //         console.log('Error with the  PUT', error)
+        //     })
+    }
+
     return(
         <li>
             {props.galleryList.description}
             {props.galleryList.likes}
+            <button onClick={likeHandler}>Love it </button>
+
         </li>
     );
 }
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -11,4 +11,4 @@ function galleryList(props){
     );
 }
 
-export default galleryList
+export default galleryList;
